df98b2270d48fdb30b2193c2085ea3370cc1d0b7
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-07 12:51:40 +0200

    feat: add  check  typeof

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
  let newArray=[]
+  if (typeof(matrix) === "undefined") {
+        return [];
+    }
       for (let i = 0; i < matrix.length; i++) {
        for (let j = 0; j < matrix[0].length; j++) {
         newArray.push(matrix[i][j])
