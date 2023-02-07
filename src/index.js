b0f08b593389b8fc634adfb38732442248891c4f
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-07 14:11:01 +0200

    feat: change mistakes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 module.exports = function towelSort (matrix) {
-let newArray = [];
-  if (typeof matrix === "undefined") {
+let newArray=[]
+  if (typeof(matrix) === "undefined") {
     return [];
-  }
+}
   for (let i = 0; i < matrix.length; i++) {
-    if (i % 2 === 0) {
-      for (let j = 0; j < matrix[0].length; j++) {
-        newArray.push(matrix[i][j]);
-      }
-    } else {
-      for (let j = matrix[0].length; j >= 0; j--) {
-        newArray.push(matrix[i][j]);
-      }
-    }
-  }
-  return newArray;
+    if(i%2!==0)matrix[i].reverse()
+   for (let j = 0; j < matrix[0].length; j++) {
+    newArray.push(matrix[i][j])}
+    }      
+  return newArray
 }
